a077f65fd21fe47c7f99a19015d58c1e1ebf37a7
shannonwells <shannonwells@users.noreply.github.com>
2024-06-27 11:36:41 -0700

    runtime api working w/ make start and make e2e tests

--- e2e/capacity/change_staking_target.test.ts
+++ e2e/capacity/change_staking_target.test.ts
@@ -5,8 +5,7 @@ import assert from "assert";
 import { ExtrinsicHelper, } from "../scaffolding/extrinsicHelpers";
 import {
   devAccounts, createKeys, createMsaAndProvider,
-  stakeToProvider, CHAIN_ENVIRONMENT,
-  TEST_EPOCH_LENGTH, setEpochLength,
+  stakeToProvider,
   CENTS, DOLLARS, createAndFundKeypair, createProviderKeysAndId
 }
   from "../scaffolding/helpers";
@@ -16,17 +15,6 @@ import { MessageSourceId} from "@frequency-chain/api-augment/interfaces";
 describe("change_staking_target tests", () => {
   const tokenMinStake: bigint = 1n * CENTS;
   const capacityMin: bigint = tokenMinStake / 50n;
-
-  const unusedMsaId = async () => {
-    const maxMsaId = (await ExtrinsicHelper.getCurrentMsaIdentifierMaximum()).toNumber();
-    return maxMsaId + 99;
-  }
-
-  before(async () => {
-    if (process.env.CHAIN_ENVIRONMENT === CHAIN_ENVIRONMENT.DEVELOPMENT) {
-      await setEpochLength(devAccounts[0].keys, TEST_EPOCH_LENGTH);
-    }
-  });
 
   it("happy path succeeds", async () => {
       const providerBalance = 2n * DOLLARS;
@@ -45,9 +33,9 @@ describe("change_staking_target tests", () => {
   it("fails if 'to' is not a Provider", async () => {
     const providerBalance = 2n * DOLLARS;
     const stakeKeys = createKeys("staker");
-    const notAProvider = await unusedMsaId();
     const oldProvider = await createMsaAndProvider(stakeKeys, "Provider1", providerBalance);
     await assert.doesNotReject(stakeToProvider(stakeKeys, oldProvider, tokenMinStake*3n));
+    const notAProvider = 3;
     const call = ExtrinsicHelper.changeStakingTarget(stakeKeys, oldProvider, notAProvider, tokenMinStake);
     await assert.rejects(call.signAndSend(), {name: "InvalidTarget"})
   });
